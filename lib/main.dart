be3283aa54d0f1e22abb9c965fc30d8de981b4fb
reda2032 <reda.ahmed.2032@gmail.com>
2025-11-20 19:38:00 +0200

    env_create done

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,22 @@
 import 'package:asroo_store_app/asroo_store_app.dart';
+import 'package:asroo_store_app/core/app/env.variables.dart';
 import 'package:asroo_store_app/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialized environment variables
+  await EnvVariable.instance.init(envType: EnvTypeEnum.prod);
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const AsrooStoreApp());
+  await SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp],
+  ).then((_) {
+    runApp(const AsrooStoreApp());
+  });
 }
